b90d17c13b426e87d6d92f4397b23a6b49326d82
Camilla Calle <camilla7732528@gmail.com>
2025-04-21 12:57:44 -0400

    Settings page

--- taskquest1/lib/screens/settings_page.dart
+++ taskquest1/lib/screens/settings_page.dart
@@ -1,6 +1,34 @@
+// import 'package:flutter/material.dart';
+// import 'components/const/colors.dart';
+// import 'avatar_design_page.dart';
+//
+// class SettingsPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView(
+//       padding: const EdgeInsets.all(16),
+//       children: [
+//         ListTile(
+//           leading: Icon(Icons.person, color: primaryGreen),
+//           title: Text('Customize Avatar'),
+//           trailing: Icon(Icons.arrow_forward_ios),
+//           onTap: () {
+//             Navigator.push(
+//               context,
+//               MaterialPageRoute(builder: (context) => AvatarDesignPage()),
+//             );
+//           },
+//         ),
+//         Divider(),
+//
+//       ],
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'components/const/colors.dart';
 import 'avatar_design_page.dart';
+// (Assuming you'll create a notification settings page later)
 
 class SettingsPage extends StatelessWidget {
   @override
@@ -20,7 +48,29 @@ class SettingsPage extends StatelessWidget {
           },
         ),
         Divider(),
-
+        ListTile(
+          leading: Icon(Icons.notifications, color: primaryGreen),
+          title: Text('Notification Permissions'),
+          trailing: Icon(Icons.arrow_forward_ios),
+          onTap: () {
+            // Later you can navigate to a NotificationSettingsPage()
+            // For now, you could just show a placeholder page or dialog
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('Notification Permissions'),
+                content: Text('Need to implement / configure for permissions I think Ammar is doing this'),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text('OK', style: TextStyle(color: primaryGreen)),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+        Divider(),
       ],
     );
   }
