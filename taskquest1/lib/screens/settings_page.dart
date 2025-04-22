addc5f4e3c1508ae316194ed4d2452ea12a51a44
Camilla Calle <camilla7732528@gmail.com>
2025-04-22 15:41:59 -0400

    Added image

--- taskquest1/lib/screens/settings_page.dart
+++ taskquest1/lib/screens/settings_page.dart
@@ -1,30 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'components/const/colors.dart';
-// import 'avatar_design_page.dart';
-//
-// class SettingsPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView(
-//       padding: const EdgeInsets.all(16),
-//       children: [
-//         ListTile(
-//           leading: Icon(Icons.person, color: primaryGreen),
-//           title: Text('Customize Avatar'),
-//           trailing: Icon(Icons.arrow_forward_ios),
-//           onTap: () {
-//             Navigator.push(
-//               context,
-//               MaterialPageRoute(builder: (context) => AvatarDesignPage()),
-//             );
-//           },
-//         ),
-//         Divider(),
-//
-//       ],
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'components/const/colors.dart';
 import 'avatar_design_page.dart';
