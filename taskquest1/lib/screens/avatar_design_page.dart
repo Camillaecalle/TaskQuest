434cff88fcfd3a2d3e09fec4233958cafbdb5af2
Camilla Calle <camilla7732528@gmail.com>
2025-04-22 12:37:21 -0400

    Avatars Added with carasoul

--- taskquest1/lib/screens/avatar_design_page.dart
+++ taskquest1/lib/screens/avatar_design_page.dart
@@ -1,20 +1,76 @@
+// import 'package:flutter/material.dart';
+// import 'components/const/colors.dart';
+//
+// class AvatarDesignPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Design Avatar'),
+//         backgroundColor: primaryGreen,
+//         centerTitle: true,
+//       ),
+//       body: Center(
+//         child: Text(
+//           'Users will be able to design avatar here',
+//           style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'components/const/colors.dart';
 
 class AvatarDesignPage extends StatelessWidget {
+  // List of avatar image paths
+  final List<String> avatarImages = [
+    'assets/avatars/turtle.png',
+    'assets/avatars/giraffe.png',
+    'assets/avatars/Cat.png',
+    'assets/avatars/dolphin.png',
+    'assets/avatars/cow.png',
+    'assets/avatars/panda.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Design Avatar'),
+        title: Text('Choose Your Avatar'),
         backgroundColor: primaryGreen,
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'Users will be able to design avatar here',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
+      body: PageView.builder(
+        itemCount: avatarImages.length,
+        itemBuilder: (context, index) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 250,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(avatarImages[index]),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Unlock with X points!', // You can replace X dynamically later
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: primaryGreen,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
